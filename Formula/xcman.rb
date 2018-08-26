1c412cb9b64ec43962853ecfd40c538e25fa585d
Josef Dolezal <pepik.ml@gmail.com>
2018-08-26 23:06:52 +0200

    Release xcman 0.0.2

--- Formula/xcman.rb
+++ Formula/xcman.rb
@@ -1,8 +1,8 @@
 class Xcman < Formula
     desc "Xcode templates and snippets manager"
     homepage "https://github.com/josefdolezal/xcman"
-    url "https://github.com/josefdolezal/xcman/archive/0.0.1.tar.gz"
-    sha256 "5b1a3fc8863bea03605d28a0eb17f8a0ae358cb3e06157aa02116c54f29ede06"
+    url "https://github.com/josefdolezal/xcman/archive/0.0.2.tar.gz"
+    sha256 "27911b4f0c7eb6ec4b9550409eddfa84a60488f76af47c059af9ac88fe5305e2"
   
     depends_on "cmake" => :build
     depends_on :xcode => ["9.0", :build]
